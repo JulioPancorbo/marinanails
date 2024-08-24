35ddefdb8942d93f1b21117cb60a3488c9ce3368
JulioPancorbo <juliopancorbo@xerintel.es>
2024-08-24 20:51:49 +0200

    Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api/',
+  // apiUrl: 'http://localhost:8000/api/',
+  apiUrl: 'https://marinanails-laravel.vercel.app/api/',
+
 };
